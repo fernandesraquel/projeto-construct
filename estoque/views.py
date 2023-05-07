97a058222121b62de76d3aca9bbdc003be4fd195
Raquel Fernandes <raquel.lima.072@ufrn.edu.br>
2023-05-07 11:58:45 -0300

    Update aula 04

--- estoque/views.py
+++ estoque/views.py
@@ -6,28 +6,36 @@ from datetime import date
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# Create your views here.
+
+
+
 
 def add_produto(request):
-    if request.method == "POST":
+    if request.method == "GET":
+        categorias = Categoria.objects.all()
+        return render(request, 'add_produto.html', {'categorias': categorias})
+    elif request.method == "POST":
         nome = request.POST.get('nome')
         categoria = request.POST.get('categoria')
         quantidade = request.POST.get('quantidade')
         preco_compra = request.POST.get('preco_compra')
         preco_venda = request.POST.get('preco_venda')
-
-        produto = Produto(nome=nome,
-                            categoria_id=categoria,
-                            quantidade=quantidade,
-                            preco_compra=preco_compra,
-                            preco_venda=preco_venda)
+        imagens = request.FILES.getlist('imagens')
         
+        produto = Produto(nome=nome, 
+                          categoria_id=categoria, 
+                          quantidade=quantidade, 
+                          preco_compra=preco_compra, 
+                          preco_venda=preco_venda
+        )
         produto.save()
         
         for f in request.FILES.getlist('imagens'):
             name = f'{date.today()}-{produto.id}.jpg'
-            
+
             img = Image.open(f)
             img = img.convert('RGB')
             img = img.resize((300, 300))
@@ -37,15 +45,14 @@ def add_produto(request):
             img.save(output, format="JPEG", quality=100)
             output.seek(0)
             img_final = InMemoryUploadedFile(output,
-                                                'ImageField',
-                                                name,
-                                                'image/jpeg',
-                                                sys.getsizeof(output),
-                                                None
+                                            'ImageField',
+                                            name,
+                                            'image/jpeg',
+                                            sys.getsizeof(output),
+                                            None
             )
-
-
+        
             img_dj = Imagem(imagem = img_final, produto=produto)
             img_dj.save()
-        return HttpResponse('foi')      
-    
+
+        return HttpResponse('foi')
